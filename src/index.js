2496147054671f362cdb4aa38573463d996037dc
RomanMillan <romandos962m@gmail.com>
2023-01-24 08:53:49 +0100

    actividad 1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react"
 import {createRoot}  from "react-dom/client";
 //import { ReactDOM } from "react-dom";
-import PrimeraApp from "./PrimeraApp";
+//import PrimeraApp from "./PrimeraApp";
+import CounterApp from "./CounterApp";
 import './index.css';
 
 const container = document.querySelector('#root');
-const saludo = <h1>Hola mundo</h1>; //<== eso es jsx
+//const saludo = <h1>Hola mundo</h1>; //<== eso es jsx
 
 const root = createRoot(container);
-root.render(<PrimeraApp nombre="Maria" apellido="Garcia" />);
+//root.render(<PrimeraApp nombre="Maria" apellido="Garcia" />);
 //ReactDOM.render(saludo, root);
+
+
+root.render(<CounterApp value={5} />);
+
